444af08c6aaf268adcdda7b7a912c79cc691be76
yusuke <yusuke.iritani0909@gmail.com>
2024-09-02 22:32:11 +0900

    refactor

--- lib/ui/page/profile/widgets/videos.dart
+++ lib/ui/page/profile/widgets/videos.dart
@@ -34,17 +34,30 @@ class _VideosPageState extends State<VideosPage> {
       child: Column(
         children: [
           Padding(
-              padding: const EdgeInsets.only(top: 8),
-              child: OrderButton(
-                  onItemTapped: _onItemTapped, selectedIndex: _selectedIndex)),
+            padding: const EdgeInsets.only(top: 8),
+            child: OrderButton(
+              onItemTapped: _onItemTapped,
+              selectedIndex: _selectedIndex,
+            ),
+          ),
           Flexible(
             child: ListView.builder(
               itemCount: 10,
               itemBuilder: (BuildContext context, int index) {
-                return VideoThumbnail(
-                    videoIndex: index,
-                    onVideoTapped: _onVideoTapped,
-                    onLeaderClicked: _onLeaderClicked);
+                // 全然よくわからんがこうしないとタップ時の色がつかない
+                return Material(
+                  color: Colors
+                      .transparent, // Set to transparent to allow ripple effect
+                  child: InkWell(
+                    onTap: () => _onVideoTapped(index),
+                    onLongPress: () => _onLeaderClicked(index),
+                    child: VideoThumbnail(
+                      videoIndex: index,
+                      onVideoTapped: _onVideoTapped,
+                      onLeaderClicked: _onLeaderClicked,
+                    ),
+                  ),
+                );
               },
             ),
           ),
